6fb3be305d1a788ca2a33284cde01063c48d84b5
Lu <SLu@tree.com>
2018-06-13 11:46:08 -0400

    1.0

---
+++ Article Recommendation/Python Scripts/test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+af = pd.read_csv("../Sample Data/Affinity.csv", header = None)
+pp = pd.read_csv("../Sample Data/Parent and Popularity.csv")
+ca = pd.read_csv("../Sample Data/Calculators.csv")
