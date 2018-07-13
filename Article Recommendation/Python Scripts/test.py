0f37b95012d8f58629949ce0e58ece7bfa7f7b42
Sang Lu <slu7@ncsu.edu>
2018-07-12 22:18:34 -0400

    2.0

--- Article Recommendation/Python Scripts/test.py
+++ Article Recommendation/Python Scripts/test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
-af = pd.read_csv("../Sample Data/Affinity.csv", header = None)
-pp = pd.read_csv("../Sample Data/Parent and Popularity.csv")
-ca = pd.read_csv("../Sample Data/Calculators.csv")
+wp = pd.read_csv("../Sample Data/WP-7-9.csv")
+ad = pd.read_csv("../Sample Data/Adobe article temp pages4.11-7.9.csv")
+
+ad['Page'] = ad['Page'] + "/"
